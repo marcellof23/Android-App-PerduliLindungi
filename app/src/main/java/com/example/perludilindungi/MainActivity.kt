227e91c5d2ce2a88c902fa58018ad36893275fb6
Farhan <ahandnr@gmail.com>
2022-03-04 23:34:46 +0700

    Tombol buat scan udah

--- app/src/main/java/com/example/perludilindungi/MainActivity.kt
+++ app/src/main/java/com/example/perludilindungi/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.perludilindungi
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -10,6 +12,8 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.fragment.app.Fragment
 
 import com.example.perludilindungi.databinding.ActivityMainBinding
+import com.example.perludilindungi.ui.checkin.CheckinActivity
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity(), IUseBottomNav {
 
@@ -34,9 +38,12 @@ class MainActivity : AppCompatActivity(), IUseBottomNav {
 
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        val fab: View = findViewById(R.id.fab)
+        fab.setOnClickListener {
+            val intent = Intent(this@MainActivity, CheckinActivity::class.java)
+            startActivity(intent)
+//            overridePendingTransition(R.anim.slide_up, R.anim.slide_down)
+        }
     }
-}
-
-class ActivityMainBinding {
-
 }
